2e194227f1f732ee41e97c9e3a5491f553f81726
BKjungle <sunmq0617@163.com>
2019-07-31 15:14:27 +0800

    Update note.h

--- Project_Practice/note.h
+++ Project_Practice/note.h
@@ -73,93 +73,93 @@ class partOne
 	}
 
 //第三章	字符串(string) 向量(vector) 数组array
-		{
+	{
 			1、命名空间： 头文件不应包含 using 生命
-				2、string 	cin >> s  // 遇空格结束输入
-				cout << s
-				getline(cin, s); //遇换行符结束一行，但不把cin中的换行符，写入s
-			s.size(); //返回值为unsigned int ，与有符号变量计算时要注意
-			字面值与string对象做加法时，确保加号两侧有一个string对象
-				string s1 = s2 + "hello" + "world"; // 此时正确
+			2、string 	cin >> s  // 遇空格结束输入
+					cout << s
+					getline(cin, s); //遇换行符结束一行，但不把cin中的换行符，写入s
+				s.size(); //返回值为unsigned int ，与有符号变量计算时要注意
+				字面值与string对象做加法时，确保加号两侧有一个string对象
+					string s1 = s2 + "hello" + "world"; // 此时正确
 
-			建议使用c++版本的c标准库头文件，例如： cctype  对应C语言中的 ctype.h
-				范围for   for (auto c : str5) // 基于范围的for语句 遍历 string字符序列。若想改变序列元素，循环变量c定义成引用类型即可
+				建议使用c++版本的c标准库头文件，例如： cctype  对应C语言中的 ctype.h
+					范围for   for (auto c : str5) // 基于范围的for语句 遍历 string字符序列。若想改变序列元素，循环变量c定义成引用类型即可
 
-				3、迭代器：string::iterator  it;
-			cout << c << endl;
-			循环读入含标点的字符串，去标点后输出：
-				for (string s; getline(cin, s); cout << endl)
-					for (auto c : s)
-						if (!ispunct(c))
-							cout << c;
-			// ** 注意范围for内的遍历序列大小不能改变
-			3、vector：其本质是模板而不是类型。能用大多数类型对象作为其元素，但不含引用(不是对象)
-				vector<int> v1;   // 默认 
-			vector<int> v1(v2); // 复制
-			vector<int> v3(3, 4); // 三个 对象，初始化值 4
-			vector<int> v4(3);  // 三个 默认初始值对象 
-			vector<int> v5{ 1,2,5,3 }; // list initialize  
-			vector<int> v6 = { 1,2,4,6 };// list initialize 或 if initialized 则为拷贝替换
+			3、迭代器：string::iterator  it;
+				cout << c << endl;
+				循环读入含标点的字符串，去标点后输出：
+					for (string s; getline(cin, s); cout << endl)
+						for (auto c : s)
+							if (!ispunct(c))
+								cout << c;
+				// ** 注意范围for内的遍历序列大小不能改变
+				3、vector：其本质是模板而不是类型。能用大多数类型对象作为其元素，但不含引用(不是对象)
+					vector<int> v1;   // 默认 
+				vector<int> v1(v2); // 复制
+				vector<int> v3(3, 4); // 三个 对象，初始化值 4
+				vector<int> v4(3);  // 三个 默认初始值对象 
+				vector<int> v5{ 1,2,5,3 }; // list initialize  
+				vector<int> v6 = { 1,2,4,6 };// list initialize 或 if initialized 则为拷贝替换
 
-			vector<string>  v6("hello "); // err, con't use literal initialize v6
-			vector<string>  v7{ 4 };  		//if 花括号内的值不能用来初始化(例如类型不同)，则考虑用这样的值来构造。此为4 个string 对象
-			vector<string>  v8{ 10,"hello" }; // 原理同上， 10个 值为 “hello”的元素
+				vector<string>  v6("hello "); // err, con't use literal initialize v6
+				vector<string>  v7{ 4 };  		//if 花括号内的值不能用来初始化(例如类型不同)，则考虑用这样的值来构造。此为4 个string 对象
+				vector<string>  v8{ 10,"hello" }; // 原理同上， 10个 值为 “hello”的元素
 
-			习题： vector<string> v8;
-			for (string buffer; cin >> buffer; v8.push_back(buffer));  // 循环输入 字符串  遇文件结束符EOF结束，windos下为ctrl+z 
+				习题： vector<string> v8;
+				for (string buffer; cin >> buffer; v8.push_back(buffer));  // 循环输入 字符串  遇文件结束符EOF结束，windos下为ctrl+z 
 
 
 			4、迭代器
-				1 - 若容器为空，begin和end返回同一iterator，尾后迭代器(end)
-				2 - *iter  // 获取所指元素的引用(即内容可更改)
-				3 - iter->mem // 获取该迭代器所指元素 内的mem元素
-				4 - ++iter, --iter// 递增 递减。移动到下一个元素 ，上一个元素
-				5 - == ， != // 判断是否只想同一元素。
-				6 - (*iter).empty(), iter->empty() // 访问对象成员
+					1 - 若容器为空，begin和end返回同一iterator，尾后迭代器(end)
+					2 - *iter  // 获取所指元素的引用(即内容可更改)
+					3 - iter->mem // 获取该迭代器所指元素 内的mem元素
+					4 - ++iter, --iter// 递增 递减。移动到下一个元素 ，上一个元素
+					5 - == ， != // 判断是否只想同一元素。
+					6 - (*iter).empty(), iter->empty() // 访问对象成员
 
-				vector<int> v1;
-			const vector<int> v2; // 若对象是常量，则begin 和end 返回const_iterator
-			auto it1 = v1.begin(); // it1 类型是 vector<int>::iterator 
-			auto it2 = v2.begin(); // it2 类型是 vector<int>::const_iterator 
-			//c11 规定
-			auto v3 = v1.cbegin(); // cbegin，cend 专门返回常量类型迭代器const_iterator
+					vector<int> v1;
+				const vector<int> v2; // 若对象是常量，则begin 和end 返回const_iterator
+				auto it1 = v1.begin(); // it1 类型是 vector<int>::iterator 
+				auto it2 = v2.begin(); // it2 类型是 vector<int>::const_iterator 
+				//c11 规定
+				auto v3 = v1.cbegin(); // cbegin，cend 专门返回常量类型迭代器const_iterator
 
-			*但凡使用了迭代器循环体，都不要向迭代器所属容器添加元素，会导致迭代器失效*
-				//遍历对象元素中的元素，变大写
-				vector<string> text;
-			for (string line; getline(cin, line); text.push_back(line));
+				*但凡使用了迭代器循环体，都不要向迭代器所属容器添加元素，会导致迭代器失效*
+					//遍历对象元素中的元素，变大写
+					vector<string> text;
+				for (string line; getline(cin, line); text.push_back(line));
 
-			for (auto& word : text)
-			{
-				for (auto& ch : word)
-					if (isalpha(ch)) ch = toupper(ch);
-				cout << word << " ";
-			}
-
-			迭代器运算：
-				iter + n // 后移	
-				iter - n // 前移
-				iter += n // 后移新位置
-				iter -= n //前移新位置
-				iter2 - iter1 // 得到的是差值 
-					> , < , >= , <= , != // 判断
-
-				auto mid = v1.begin() + v1.size() / 2;
-			if (iter < mid)  // 处理前半部分元素
-
-				3.4.1  // redo the 3.3.3  use iterator
-				vector<string> v6;
-			for (string str; cin >> str; v6.push_back(str));
-
-			for (auto beg = v6.begin(); beg != v6.end(); beg++)
-			{
-				for (auto c = (*beg).begin(); c != (*beg).end(); c++)
+				for (auto& word : text)
 				{
-					*c = toupper(*c);
+					for (auto& ch : word)
+						if (isalpha(ch)) ch = toupper(ch);
+					cout << word << " ";
 				}
-				cout << *beg << endl;
-			}
 
-		}
+			3.4.2	迭代器运算：
+					iter + n // 后移	
+					iter - n // 前移
+					iter += n // 后移新位置
+					iter -= n //前移新位置
+					iter2 - iter1 // 得到的是差值 
+						> , < , >= , <= , != // 判断
+
+					auto mid = v1.begin() + v1.size() / 2;
+				if (iter < mid)  // 处理前半部分元素
+
+			  // redo the 3.3.3  use iterator
+					vector<string> v6;
+				for (string str; cin >> str; v6.push_back(str));
+
+				for (auto beg = v6.begin(); beg != v6.end(); beg++)
+				{
+					for (auto c = (*beg).begin(); c != (*beg).end(); c++)
+					{
+						*c = toupper(*c);
+					}
+					cout << *beg << endl;
+				}
+
+	}
 
 }
