b3ccbe0dae9acf61520559879f0beaff59b444da
MQ_sun <sunonms@outlook.com>
2019-07-31 20:12:06 +0800

    something

--- Project_Practice/T.cpp
+++ Project_Practice/T.cpp
@@ -5,7 +5,8 @@
 int main()
 {
 	std::cout << "Hello World ~" << std::endl;
-	
+	float weight = static_cast<float>(128.0) / static_cast<float>(256.0);
+	std::cout << "weight:" << weight << "--"<< int(abs(0.8)) << std::endl;
 	show();
 	return 0;
 }
