3e1e2ac17828de8a86878258c3b98fae44f091e9
sun <sun@DESKTOP-MDUEOUP>
2019-09-26 00:12:19 +0800

    ss

--- Project_Practice/ChapterNine_sc.h
+++ Project_Practice/ChapterNine_sc.h
@@ -1,6 +1,15 @@
 #pragma once
 #include "stdafx.h"
+void double_and_insert(std::vector<int>& v, int some_val)
+{
 
+	auto mid = [&] { return v.begin() + v.size() / 2; };
+
+	for (auto curr = v.begin(); curr != mid(); ++curr)
+		if (*curr == some_val)
+			++(curr = v.insert(curr, 2 * some_val));
+
+}
 
 class ChapterNine
 {
@@ -50,8 +59,21 @@ public:
 		}
 
 			break;
+		case 3:
+		{
+			std::vector<int> v{ 6, 9, 3, 9, 9, 9, 3, 1 ,4,5,6,7};
+
+			double_and_insert(v, 1);
+
+			for (auto i : v)
+				std::cout << i << std::endl;
+		}
+		
+			break;
 		default:
 			;
 		}
 	}
 };
+
+
